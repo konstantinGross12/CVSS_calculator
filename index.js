693d359bc9e99631a1b670ff0b0f38092fbc56c6
Konstantin Gross <gross.konstantin.1987@gmail.com>
2025-02-04 09:47:54 +0100

    base vars defined

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ import * as data from './data/data.js';
 console.log('Hello World');
 console.log(data.data);
 console.log(data.av);
+
+let CVSS_base_score;
+let impact_sub_score;
+let exploitability_sub_score;
+let CVSS_temporal_score;
+let CVSS_environmental_score;
+let CVSS_modified_score;
+let Overall_CVSS_score;
+
+let Overall_CVSS_vector;
