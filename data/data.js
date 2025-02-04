1b1b32ad077fcf134c1aea2d5fcb295014360f79
Konstantin Gross <gross.konstantin.1987@gmail.com>
2025-02-04 09:39:48 +0100

    Initial commit

---
+++ data/data.js
@@ -0,0 +1,14 @@
+export const data = ['a', 'b', 'c'];
+
+export const av = [
+  {
+    label: 'Network',
+    value_calcultation: 0.85,
+    dataverse_label: 'Network',
+    dataverse_value: 775000000,
+    string_value: 'AV:N',
+  },
+  {},
+  {},
+  {},
+];
---
+++ index.js
@@ -0,0 +1,5 @@
+import * as data from './data/data.js';
+
+console.log('Hello World');
+console.log(data.data);
+console.log(data.av);
