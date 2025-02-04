b439195794e66d1cbe188c99812e334c927f7b54
S04-Ivy <ivymax@web.de>
2025-02-04 16:03:07 +0100

    Base Added

--- data/data.js
+++ data/data.js
@@ -1,4 +1,5 @@
-//todo finalize all cvss objects
+//BASE
+//Attack Vector
 export const av = [
   {
     label: "Network",
@@ -14,17 +15,149 @@ export const av = [
     dataverse_value: 775000001,
     string_value: "AV:A",
   },
-  {},
-
-  {},
+  {
+    label: "Local",
+    value_calcultation: 0.55,
+    dataverse_label: "Local",
+    dataverse_value: 775000002,
+    string_value: "AV:L",
+  },
+  {
+    label: "Physical",
+    value_calcultation: 0.2,
+    dataverse_label: "Physical",
+    dataverse_value: 775000003,
+    string_value: "AV:P",
+  },
 ];
+//Attack Complexity
 export const ac = [
   {
-    label: "Network",
-    value_calcultation: 0.85,
-    dataverse_label: "Network",
+    label: "Low",
+    value_calcultation: 0.77,
+    dataverse_label: "Low",
     dataverse_value: 775000000,
-    string_value: "AV:N",
+    string_value: "AC:L",
   },
-  {},
+  {
+    label: "High",
+    value_calcultation: 0.44,
+    dataverse_label: "High",
+    dataverse_value: 775000001,
+    string_value: "AC:H",
+  },
+];
+//Privileges Required
+export const pr = [
+  {
+    label: "None",
+    value_calcultation: 0.85,
+    dataverse_label: "None",
+    dataverse_value: 775000000,
+    string_value: "PR:N",
+  },
+  {
+    label: "Low",
+    value_calcultation: 0.62, //!!0.62 needs to be discussed!!
+    dataverse_label: "Low",
+    dataverse_value: 775000001,
+    string_value: "PR:L",
+  },
+  {
+    label: "High",
+    value_calcultation: 0.27, //!!0.27  needs to be discussed!!
+    dataverse_label: "High",
+    dataverse_value: 775000002,
+    string_value: "PR:H",
+  },
+];
+//User Interaction
+export const ui = [
+  {
+    label: "None",
+    value_calcultation: 0.85,
+    dataverse_label: "None",
+    dataverse_value: 775000000,
+    string_value: "UI:N",
+  },
+  {
+    label: "Required",
+    value_calcultation: 0.62,
+    dataverse_label: "Required",
+    dataverse_value: 775000001,
+    string_value: "UI:R",
+  },
+];
+//Scope is missing in the CVSS Docu
+//Confidentiality Impact
+export const c = [
+  {
+    label: "High",
+    value_calcultation: 0.56,
+    dataverse_label: "High",
+    dataverse_value: 775000002,
+    string_value: "C:H",
+  },
+  {
+    label: "Low",
+    value_calcultation: 0.22,
+    dataverse_label: "Low",
+    dataverse_value: 775000001,
+    string_value: "C:L",
+  },
+  {
+    label: "None",
+    value_calcultation: 0,
+    dataverse_label: "None",
+    dataverse_value: 775000000,
+    string_value: "C:N",
+  },
+];
+// Integrity Impact
+export const i = [
+  {
+    label: "High",
+    value_calcultation: 0.56,
+    dataverse_label: "High",
+    dataverse_value: 775000002,
+    string_value: "I:H",
+  },
+  {
+    label: "Low",
+    value_calcultation: 0.22,
+    dataverse_label: "Low",
+    dataverse_value: 775000001,
+    string_value: "I:L",
+  },
+  {
+    label: "None",
+    value_calcultation: 0,
+    dataverse_label: "None",
+    dataverse_value: 775000000,
+    string_value: "I:N",
+  },
+];
+//Availability Impact
+export const a = [
+  {
+    label: "High",
+    value_calcultation: 0.56,
+    dataverse_label: "High",
+    dataverse_value: 775000002,
+    string_value: "A:H",
+  },
+  {
+    label: "Low",
+    value_calcultation: 0.22,
+    dataverse_label: "Low",
+    dataverse_value: 775000001,
+    string_value: "A:L",
+  },
+  {
+    label: "None",
+    value_calcultation: 0,
+    dataverse_label: "None",
+    dataverse_value: 775000000,
+    string_value: "A:N",
+  },
 ];
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import * as data from "./data/data.js";
 
-//console.log('Hello World');
-console.log("Here I display Attack Vector");
+console.log("Base");
+console.log("Attack Vector");
 console.log(data.av);
-console.log("--------------------------------");
-console.log("Here I display Attack Complexity");
+console.log("Attack Complexity");
 console.log(data.ac);
+console.log("Privileges Required");
+console.log(data.pr);
+console.log("User Interaction");
+console.log(data.ui);
+console.log("Scope is mssing in the CVSS Docu");
+console.log("Confidentiality Impact");
+console.log(data.c);
+console.log("Integrity Impact");
+console.log(data.i);
+console.log("Availability Impact");
+console.log(data.a);
+console.log("--------------------------------");
+console.log("Environmental");
 
 let CVSS_base_score;
 let impact_sub_score;
