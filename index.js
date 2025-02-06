2d910dcb8f51731d295304ca72795af0128253bc
S04-Ivy <ivymax@web.de>
2025-02-06 15:55:37 +0100

    Score Calculation Description Started

--- index.js
+++ index.js
@@ -60,12 +60,49 @@ console.log(data.ir);
 console.log("Availability Requirement (AR)");
 console.log(data.ar);
 
+console.log("Base Score Calculation");
+// Consits of three calculated values: Impact Sub Score (ISS), Impact & Exploitability
+// 1) ISS = 	1 - [ (1 - Confidentiality) × (1 - Integrity) × (1 - Availability) ]
+
+// 2) Impact =
+// If Scope is Unchanged:	6.42 × ISS
+// If Scope is Changed	7.52 × (ISS - 0.029) - 3.25 × (ISS - 0.02)hoch 15
+
+// 3) Exploitability = 	8.22 × AttackVector × AttackComplexity × 	PrivilegesRequired × UserInteraction
+
+// Ends up in Base Score:
+// If Impact \<= 0 --> 	0, else
+// If Scope is Unchanged -->	Roundup (Minimum [(Impact + Exploitability), 10])
+// If Scope is Changed -->	Roundup (Minimum [1.08 × (Impact + Exploitability), 10])
+
+//FRAGE: Was ist Scope changed?? Es ist nicht!! Modified Scope!!
+
 let CVSS_base_score;
 let impact_sub_score;
 let exploitability_sub_score;
+
+console.log("Temporal Score Calculation");
+// Consits of one calculation
+// 1) TemporalScore =	Roundup (BaseScore × ExploitCodeMaturity × RemediationLevel × ReportConfidence)
+
 let CVSS_temporal_score;
+
+console.log("Environmental Score Calculation");
+// Consits of three calculated values: Modified Impact Sub-Score(MISS), ModifiedImpact & ModifiedExploitability
+// 1) MISS =	Minimum ( 1 - [ (1 - ConfidentialityRequirement × ModifiedConfidentiality) × (1 - IntegrityRequirement × ModifiedIntegrity) × (1 - AvailabilityRequirement × ModifiedAvailability) ], 0.915)
+
+// 2) ModifiedImpact =
+// If ModifiedScope is Unchanged	6.42 × MISS
+// If ModifiedScope is Changed	7.52 × (MISS - 0.029) - 3.25 × (MISS × 0.9731 - 0.02)hoch13 (für 3.1 differs to 3.0 vector type)
+
+// 3) ModifiedExploitability =	8.22 × ModifiedAttackVector × ModifiedAttackComplexity × ModifiedPrivilegesRequired × ModifiedUserInteraction
+
+//Ends up in Environmental Score: TBD
+
+//FRAGE: Was machen wir mit verschiedenen Vector Typen?
+
 let CVSS_environmental_score;
 let CVSS_modified_score;
-let Overall_CVSS_score;
 
+let Overall_CVSS_score;
 let Overall_CVSS_vector;
