90ad741a3802aca7f860f5f7b84e3da9b8f9e85e
S04-Ivy <ivymax@web.de>
2025-02-18 09:09:07 +0100

    Update Base, Temporal & Environmental

--- helper.js
+++ helper.js
@@ -8,11 +8,30 @@ export const input_base_full_randomzer = function (input_data) {
 
   return result;
 };
-//todo: implement the rest of the functions
-export const input_base_temporal_full_randomzer = function (input_data) {};
 
-//todo: implement the rest of the functions
-export const input_base_temporal_environmental_full_randomzer = function (input_data) {};
+export const input_base_temporal_full_randomzer = function (input_data) {
+  let result = [];
+
+  for (const input_element of input_data) {
+    const random_index = Math.floor(Math.random() * input_element.length);
+    result.push(input_element[random_index]);
+  }
+
+  return result;
+};
+
+export const input_base_temporal_environmental_full_randomzer = function (
+  input_data
+) {
+  let result = [];
+
+  for (const input_element of input_data) {
+    const random_index = Math.floor(Math.random() * input_element.length);
+    result.push(input_element[random_index]);
+  }
+
+  return result;
+};
 
 export const Minimum = function (val_1, val_2) {
   return val_1 < val_2 ? val_1 : val_2;
@@ -22,12 +41,16 @@ export const Roundup = function (val) {
   return Math.ceil(val);
 };
 
-export const calculate_impact_sub_score = function (confidentiality, integrity, availability) {
+export const calculate_impact_sub_score = function (
+  confidentiality,
+  integrity,
+  availability
+) {
   return 1 - (1 - confidentiality) * (1 - integrity) * (1 - availability);
 };
 
 export const calculate_Overall_CVSS_vector = function (input, data) {
-  let result = '';
+  let result = "";
   for (let index = 0; index < input.length; index++) {
     const element = input[index];
 
