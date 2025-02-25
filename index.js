c1b5ac783667708d1570b92d77c9599bced7a00b
Konstantin Gross <gross.konstantin.1987@gmail.com>
2025-02-25 10:18:47 +0100

    API calls

--- index.js
+++ index.js
@@ -13,9 +13,28 @@ if (!is_valid_input) {
   console.log('Mandatory input is present');
 
   let Overall_CVSS_vector = helper.calculate_Overall_CVSS_vector(input, data);
-  console.log(Overall_CVSS_vector.vector);
+  //console.log(Overall_CVSS_vector.vector);
 
-  console.log(`BaseScore is  ${Overall_CVSS_vector.BaseScore}`);
-  console.log(`TemporalScore is  ${Overall_CVSS_vector.TemporalScore}`);
-  console.log(`EnvironmentalScore is  ${Overall_CVSS_vector.EnvironmentalScore}`);
+  //console.log(`BaseScore is  ${Overall_CVSS_vector.BaseScore}`);
+  //console.log(`TemporalScore is  ${Overall_CVSS_vector.TemporalScore}`);
+  //console.log(`EnvironmentalScore is  ${Overall_CVSS_vector.EnvironmentalScore}`);
 }
+
+(async () => {
+  try {
+    // https://cvss-vector-validator-bxdqdebdh0hcb6cz.germanywestcentral-01.azurewebsites.net/v3?vector=CVSS%3A3.0%2FAV%3AN%2FAC%3AL%2FPR%3AN%2FUI%3AR%2FS%3AC%2FC%3AH%2FI%3AH%2FA%3AH%2FE%3AU%2FRL%3AO%2FRC%3AC
+    const res = await fetch(
+      //'https://cvss-vector-validator-bxdqdebdh0hcb6cz.germanywestcentral-01.azurewebsites.net/v3?vector=CVSS%3A3.0%2FAV%3AN%2FAC%3AL%2FPR%3AN%2FUI%3AR%2FS%3AC%2FC%3AH%2FI%3AH%2FA%3AH%2FE%3AU%2FRL%3AO%2FRC%3AC'
+      'https://cvss-vector-validator-bxdqdebdh0hcb6cz.germanywestcentral-01.azurewebsites.net/v3?vector=CVSS:3.0/AV:P/AC:H/PR:N/UI:N/S:C/C:N/I:L/A:H/E:F/RL:X/RC:C/CR:M/IR:X/AR:L/MAV:X/MAC:H/MPR:H/MUI:N/MS:X/MC:N/MI:X/MA:N'
+    );
+    const headerDate = res.headers && res.headers.get('date') ? res.headers.get('date') : 'no response date';
+    console.log('Status Code:', res.status);
+    console.log('Date in Response header:', headerDate);
+
+    const result = await res.json();
+
+    console.log(result);
+  } catch (err) {
+    console.log(err.message); //can be console.error
+  }
+})();
